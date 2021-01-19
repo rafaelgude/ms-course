c28f00b75fad7f8ba558a16319d5bc5be1af3220
Rafael Gude <gudemvl@hotmail.com>
2021-01-18 22:50:39 -0300

    Rest Template

--- hr-payroll/src/main/java/com/rafaelgude/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rafaelgude/hrpayroll/services/PaymentService.java
@@ -1,14 +1,27 @@
 package com.rafaelgude.hrpayroll.services;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.rafaelgude.hrpayroll.entities.Payment;
+import com.rafaelgude.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		var worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, Map.of("id", workerId));
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }
