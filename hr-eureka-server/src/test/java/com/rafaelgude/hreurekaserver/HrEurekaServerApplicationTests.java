f56bf5b7c278605e92f5203d31060c9b32c67a2b
Rafael Gude <gudemvl@hotmail.com>
2021-01-19 22:14:43 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/rafaelgude/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelgude.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
