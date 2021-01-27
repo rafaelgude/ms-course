50b0935b10598fd74e8ce3b2736c5981c508cd41
Rafael Gude <gudemvl@hotmail.com>
2021-01-26 21:00:32 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/rafaelgude/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/rafaelgude/hroauth/services/UserService.java
@@ -2,13 +2,16 @@ package com.rafaelgude.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.rafaelgude.hroauth.entities.User;
 import com.rafaelgude.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger Logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 
@@ -20,11 +23,16 @@ public class UserService {
 		
 		if (user == null) {
 			Logger.error("Email not found: " + email);
-			throw new IllegalArgumentException("Email not found.");
+			throw new UsernameNotFoundException("Email not found.");
 		}
 		
 		Logger.info("Email found: " + email);
 		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return findByEmail(username);
 	}
 
 }
