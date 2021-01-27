14ceb8db2304b2863ef385e6b15fd6a3acd46ce8
Rafael Gude <gudemvl@hotmail.com>
2021-01-27 20:29:26 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/rafaelgude/hrapigatewayzuul/configs/AppConfig.java
@@ -0,0 +1,24 @@
+package com.rafaelgude.hrapigatewayzuul.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		var tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+}
